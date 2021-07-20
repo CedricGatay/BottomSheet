98fafe5d8e54d2b39cfce7d7720f297a60e10164
Cedric Gatay <c.gatay@code-troopers.com>
2021-07-20 18:07:07 +0200

    hotfix(): xcode 12 does not like iOSApplicationExtension
    Replaced conditional checks for iOSApplicationExtension with iOS checks

--- Sources/BottomSheet/UIKitExtensions.swift
+++ Sources/BottomSheet/UIKitExtensions.swift
@@ -2,15 +2,15 @@
 import UIKit
 extension UIView {
     var viewSafeAreaInsets: UIEdgeInsets {
-        if #available(iOSApplicationExtension 11.0, *) {
+        if #available(iOS 11.0, *) {
             return safeAreaInsets
-        } else {
+        } else  {
             return UIEdgeInsets(top: 0, left: 0, bottom: 0, right: 0)
         }
     }
 
     var viewSafeAreaLayoutGuide: UILayoutGuide {
-        if #available(iOSApplicationExtension 11.0, *) {
+        if #available(iOS 11.0, *) {
             return safeAreaLayoutGuide
         } else {
             return layoutMarginsGuide
